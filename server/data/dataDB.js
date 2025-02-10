f8d2c31796f55e39f20a6f7ddcc962da317fbc51
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-02-10 18:51:12 +0200

    added db connection

---
+++ server/data/dataDB.js
@@ -0,0 +1,18 @@
+import pg from 'pg'
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: "../development.env"
+});
+const {Pool, Client} = pg;
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+});
+
+export default pool;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import pool from "./data/dataDB.js";
+
+
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json()); 
@@ -9,6 +13,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Fishing App API is running 🎣");
 });
+
+(async ()=> {
+    try{
+        const result = await pool.query("SELECT NOW()");
+        console.log("Database connected! curr time:", result.rows[0].now);
+    }catch (err){
+        console.error(err);
+    }finally {
+        pool.end();
+    }
+})();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
