f5e4ff357d378cca3b01c66a687fe6bacadb0836
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-03-21 14:09:30 +0200

    sidebar

--- client/src/Components/SideBar.jsx
+++ client/src/Components/SideBar.jsx
@@ -5,7 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
+//import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -54,26 +54,7 @@ export default function SideBar(){
 
                 </List>
             </Drawer>
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-            >
-                <Toolbar />
-                <Typography sx={{ marginBottom: 2 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-                </Typography>
-            </Box>
+
         </Box>
     )
 }
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,14 +1,17 @@
 
 import Hbimg from "../Components/Hbimg"
+import SideBar from "../Components/SideBar"
 
 export function Profile() {
     console.log("Profiili")
   return (
     <Hbimg>
+      <SideBar> 
         <div>
           <h1>Profiiliin</h1>
           <h2>Tähän lisätään omia vehkeitä ja kuvia limaisista saaliista</h2>
         </div>
+      </SideBar>
     </Hbimg>
 
   )
